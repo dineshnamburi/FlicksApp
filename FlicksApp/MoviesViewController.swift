ab861f32036fd47b0482126b742bc70ebb8833e7
Dinesh Namburi <n.dinesh36@gmail.com>
2017-02-05 18:07:47 -0500

    Made images to fade in,on load

--- FlicksApp/MoviesViewController.swift
+++ FlicksApp/MoviesViewController.swift
@@ -112,24 +112,49 @@ class MoviesViewController: UIViewController, UICollectionViewDataSource,UISearc
         poster_path = "https://image.tmdb.org/t/p/w342" + poster_path
         
         let cell = CollectView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionCell", for: indexPath) as! MovieCollectionCell
-        let img_url = URL(string: poster_path)
-        cell.movieImage.setImageWith(img_url!)
+        //let img_url = URL(string: poster_path)
+        let imageRequest = NSURLRequest(url: NSURL(string: poster_path)! as URL)
+        //cell.movieImage.setImageWith(img_url!)
+        cell.movieImage.setImageWith(
+            imageRequest as URLRequest,
+            placeholderImage: nil,
+            success: { (imageRequest, imageResponse, image) -> Void in
+                
+                // imageResponse will be nil if the image is cached
+                if imageResponse != nil {
+                    print("Image was NOT cached, fade in image")
+                    cell.movieImage.alpha = 0.0
+                    cell.movieImage.image = image
+                    UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                        cell.movieImage.alpha = 1.0
+                    })
+                } else {
+                    print("Image was cached so just update the image")
+                    cell.movieImage.image = image
+                }
+        },
+            failure: { (imageRequest, imageResponse, error) -> Void in
+                // do something for the failure condition
+        })
         return cell
     }
 
     public func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
         var temp: [NSDictionary] = []
-        
-        for movie in movies!{
-                //print ("first succ \(movie)")
-            let title = movie["title"] as! String
-            if title.lowercased().range(of: searchText.lowercased()) != nil{
-                print ("\(movie)")
-                temp.append(movie)
-            }
+        if(searchText.isEmpty){
+            self.filteredData = self.movies
         }
-        self.filteredData = temp
-        print("\(temp)")
+        else{
+            for movie in movies!{
+                //print ("first succ \(movie)")
+                let title = movie["title"] as! String
+                if title.lowercased().range(of: searchText.lowercased()) != nil{
+                    print ("\(movie)")
+                    temp.append(movie)
+                }
+            }
+            self.filteredData = temp
+        }
         self.CollectView.reloadData()
         
     }
